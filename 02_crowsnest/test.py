1bf6b172b7704bcb5c9eeb5fc675930064594734
fidodogstoevsky <88008476+fidodogstoevsky@users.noreply.github.com>
2026-02-15 12:51:43 +0100

    finished crows nest, did some of the additional ones

--- 02_crowsnest/test.py
+++ 02_crowsnest/test.py
@@ -17,14 +17,14 @@ template = 'Ahoy, Captain, {} {} off the larboard bow!'
 
 # --------------------------------------------------
 def test_exists():
-    """exists"""
+    """ checks if the file exists """
 
     assert os.path.isfile(prg)
 
 
 # --------------------------------------------------
 def test_usage():
-    """usage"""
+    """ checks if the program outputs usage instructions """
 
     for flag in ['-h', '--help']:
         rv, out = getstatusoutput(f'{prg} {flag}')
@@ -34,7 +34,7 @@ def test_usage():
 
 # --------------------------------------------------
 def test_consonant():
-    """brigantine -> a brigantine"""
+    """ brigantine -> a brigantine """
 
     for word in consonant_words:
         out = getoutput(f'{prg} {word}')
@@ -43,7 +43,7 @@ def test_consonant():
 
 # --------------------------------------------------
 def test_consonant_upper():
-    """brigantine -> a Brigatine"""
+    """ Brigantine -> a Brigantine """
 
     for word in consonant_words:
         out = getoutput(f'{prg} {word.title()}')
@@ -52,7 +52,7 @@ def test_consonant_upper():
 
 # --------------------------------------------------
 def test_vowel():
-    """octopus -> an octopus"""
+    """ octopus -> an octopus """
 
     for word in vowel_words:
         out = getoutput(f'{prg} {word}')
@@ -61,8 +61,27 @@ def test_vowel():
 
 # --------------------------------------------------
 def test_vowel_upper():
-    """octopus -> an Octopus"""
+    """ Octopus -> an Octopus """
 
     for word in vowel_words:
         out = getoutput(f'{prg} {word.upper()}')
         assert out.strip() == template.format('an', word.upper())
+
+# --------------------NEW TESTS---------------------
+
+# # --------------------------------------------------
+# def test_consonant_upper_article():
+#     """ Brigantine -> A Brigantine """
+
+#     for word in consonant_words:
+#         out = getoutput(f'{prg} {word.title()}')
+#         assert out.strip() == template.format('A', word.title())
+
+# # --------------------------------------------------
+# def test_vowel_upper_article():
+#     """ Octopus -> an Octopus """
+
+#     for word in vowel_words:
+#         out = getoutput(f'{prg} {word.upper()}')
+#         assert out.strip() == template.format('An', word.upper())
+
