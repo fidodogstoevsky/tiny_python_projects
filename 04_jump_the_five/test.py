29cccdf924163ae3b1c1e9def0ade31e63317a10
fidodogstoevsky <88008476+fidodogstoevsky@users.noreply.github.com>
2026-02-15 17:00:51 +0100

    finished the main work, implemented a test for written digits and tried but didn't finish implementing that functionality

--- 04_jump_the_five/test.py
+++ 04_jump_the_five/test.py
@@ -40,3 +40,13 @@ def test_02():
     rv, out = getstatusoutput(f'{prg} "That number to call is 098-765-4321."')
     assert rv == 0
     assert out.rstrip() == 'That number to call is 512-340-6789.'
+
+# --------------------------------------------------
+def test_num_to_string():
+    """ check if numbers are encoded into strings """
+
+    for flag in ['-w', '--write']:
+        rv, out = getstatusoutput(f'{prg} {flag} "That number to call is 098-765-4321."')
+        assert rv == 0
+        assert out.rstrip() == 'That number to call is five one two, three four zero, six seven eight nine.'
+        #assert out.rstrip() == 'That number to call is five one two three four zero six seven eight nine.'
