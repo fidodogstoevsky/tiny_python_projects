0412166c5ace5d4c4ccc07508a4f6c128ed79b89
fidodogstoevsky <88008476+fidodogstoevsky@users.noreply.github.com>
2026-02-15 14:47:20 +0100

    finished picnic, did the additional stuff

--- 03_picnic/test.py
+++ 03_picnic/test.py
@@ -66,3 +66,21 @@ def test_more_than_two_sorted():
     out = getoutput(f'{prg} {arg} --sorted')
     expected = ('You are bringing apples, bananas, cherries, and dates.')
     assert out.strip() == expected
+
+# --------------------------------------------------
+def test_no_oxford():
+    """ check if including flag -o removes the oxford comma """
+    
+    arg = 'bananas apples dates cherries'
+    out = getoutput(f'{prg} {arg} --oxford')
+    expected = ('You are bringing bananas, apples, dates and cherries.')
+    assert out.strip() == expected
+
+# --------------------------------------------------
+def test_custom_punctuation():
+    """ check if user can select custom punctuation for separating list items """
+
+    arg = 'bananas apples dates cherries'
+    out = getoutput(f'{prg} {arg} --punc ";"')
+    expected = (('You are bringing bananas; apples; dates; and cherries.'))
+    assert out.strip() == expected
