821e0b69060d5bbd787c31cebe79216cf6c4951a
fidodogstoevsky <88008476+fidodogstoevsky@users.noreply.github.com>
2026-02-16 13:52:03 +0100

    wrote ken's low memory version of howler

---
+++ 05_howler/howler_low_mem.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+"""
+low-memory howler
+"""
+
+import argparse
+import os
+import io
+import sys
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='Howler (upper-cases input)',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('text',
+                        metavar='text',
+                        type=str,
+                        help='Input string or file')
+
+    parser.add_argument('-o',
+                        '--outfile',
+                        help='Output filename',
+                        metavar='str',
+                        type=str,
+                        default='')
+
+    args = parser.parse_args()
+    
+    if os.path.isfile(args.text):
+        # if args.text is a file, replace it
+        # with an open file handle for it
+        args.text = open(args.text)
+    else:
+        # otherwise, replace it with an io.StringIO()
+        # this value acts like an open file handle
+        args.text = io.StringIO(args.text + '\n')
+    
+    return args
+
+
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+    out_fh = open(args.outfile, 'wt') if args.outfile else sys.stdout
+    for line in args.text:
+        # read the input line by line
+        # regardless of if the initial input was a filename or text
+        # they're now both formatted as io.StringIO() values
+        # so we can loop through and read each line
+        out_fh.write(line.upper())
+    out_fh.close()
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
